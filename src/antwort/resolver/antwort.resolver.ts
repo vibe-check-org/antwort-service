b37a5306aab094a034ba04889c29f3eee88e0e29
Caleb-Script <caleb-script@outlook.de>
2025-06-09 14:55:02 +0200

    update

--- src/antwort/resolver/antwort.resolver.ts
+++ src/antwort/resolver/antwort.resolver.ts
@@ -2,6 +2,7 @@ import { Mutation, Args, Resolver, Query } from '@nestjs/graphql';
 import { AntwortService } from '../service/antwort.service.js';
 import { Antwort } from '../model/entity/antwort.entity.js';
 import { CreateAntwortInput } from '../model/dto/create-antwort.input.js';
+import { Roles } from 'nest-keycloak-connect';
 
 @Resolver(() => Antwort)
 export class AntwortResolver {
@@ -11,11 +12,13 @@ export class AntwortResolver {
     }
 
     @Mutation(() => Antwort)
+    @Roles({ roles: ['Admin', 'User'] })
     createAntwort(@Args('input') input: CreateAntwortInput) {
         return this.#service.create(input);
     }
 
     @Query(() => [Antwort])
+    @Roles({ roles: ['Admin', 'User'] })
     antwortenVonUser(@Args('userId') userId: string) {
         return this.#service.findAllByUser(userId);
     }
